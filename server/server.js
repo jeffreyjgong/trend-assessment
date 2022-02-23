86787ec82ef337081eaa467af3687264ec3e8d7a
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-02-23 10:36:03 -0500

    Change to buildPath

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
+const buildPath = path.join(__dirname, '../', 'build');
 
-app.use(express.static(publicPath));
+app.use(express.static(buildPath));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(port, () => {
